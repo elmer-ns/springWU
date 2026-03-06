1f03afe52c65e936da01bb169710a4d870566baa
Elmer Nolgren Saldert <174507028+elmer-ns@users.noreply.github.com>
2026-03-06 13:10:53 +0100

    skapa about.html och basic boxlayout för den

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-let button = document.querySelector(".menu-button");
+let button = document.querySelector("#menu-button");
 button.addEventListener("click", toggleMenu);
 
-let fade_layer = document.querySelector(".fade-layer");
+let fade_layer = document.querySelector("#fade-layer");
 fade_layer.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    let menu = document.querySelector(".menu");
+    let menu = document.querySelector("#nav-menu");
     menu.classList.toggle("visible");
 
-    let fade = document.querySelector(".fade-layer");
+    let fade = document.querySelector("#fade-layer");
     fade.classList.toggle("visible");
 }
