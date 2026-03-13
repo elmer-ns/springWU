0cd141dd6bc5f201880423fa8f9908deadd41ca9
Elmer Nolgren Saldert <174507028+elmer-ns@users.noreply.github.com>
2026-03-13 12:58:05 +0100

    FAQ knappar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-let button = document.querySelector("#menu-button");
-button.addEventListener("click", toggleMenu);
+// Hamburgarmeny
+
+let menu_button = document.querySelector("#menu-button");
+menu_button.addEventListener("click", toggleMenu);
 
 let fade_layer = document.querySelector("#fade-layer");
 fade_layer.addEventListener("click", toggleMenu);
@@ -10,4 +12,18 @@ function toggleMenu() {
 
     let fade = document.querySelector("#fade-layer");
     fade.classList.toggle("visible");
+}
+
+// FAQ
+
+let faq_items = document.querySelectorAll(".faq-item h3");
+
+faq_items.forEach(function (faq_item) {
+    faq_item.addEventListener("click", toggleAnswer);
+})
+
+console.log("testturwgugest");
+
+function toggleAnswer() {
+    this.parentElement.classList.toggle("open");
 }
